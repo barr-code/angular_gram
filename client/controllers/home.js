9f875efde0187cf483886bc09ca0bc708c4ad826
barr-code <vb329@georgetown.edu>
2014-12-29 16:55:33 -0500

    Bootstrapping complete and controllers + views created; on to Satellizer implementation

---
+++ client/controllers/home.js
@@ -0,0 +1,12 @@
+angular.module('Angular-Gram')
+.controller('HomeCtrl', function($scope) {
+
+  $scope.isAuthenticated = function() {
+    // check if logged in
+  };
+
+  $scope.linkInstagram = function() {
+    // connect email account with instagram
+  };
+
+});
